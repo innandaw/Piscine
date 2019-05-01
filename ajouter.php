5c44236389ecfc1581a41d3c349ec09b5d95538a
sarahfng <sarahfong@outlook.fr>
2019-05-01 13:56:28 +0200

    recup_img

--- ajouter.php
+++ ajouter.php
@@ -8,18 +8,37 @@
 	$description=isset($_POST["description"])?$_POST["description"]:"";
 	$cat=isset($_POST["categorie"])?$_POST["categorie"]:"";
 	$sous_cat=isset($_POST["sous_cat"])?$_POST["sous_cat"]:"";
-	$sexe=isset($_POST[""])?$_POST[""]:"";
 	$nb_tailles=isset($_POST["taille"])?$_POST["taille"]:"";
+
+	$fichier_nom = $_FILES['fichier_upload']['name'];
+
+
+	$sexe = NULL;
 	
-	
+	if ($_POST['sexe'] == 'Femme')
+		{
+			$sexe = "F";
+			echo "$sexe";
+		}
+	else 
+	{
+		$sexe = "H";
+		echo "$sexe";
+	}
+
+
 	$dbh=mysqli_connect($server, $user_name, $password, $database);
 	if($dbh) {echo "Connection to the Server opened";}
 
 	
-	$sql = "INSERT INTO articles VALUES(NULL, '$nom', NULL, '$description', NULL, '$cat', NULL, NULL, '$sous_cat', NULL, NULL, NULL);";
+	for ($i = 1; $i <= $nb_tailles ; $i++)
+	{
+		$sql = "INSERT INTO articles VALUES(NULL, '$nom', '$fichier_nom', '$description', NULL, '$cat', NULL, NULL, '$sous_cat', NULL, '$sexe', NULL);";
+		$result = mysqli_query($dbh,$sql);
+	}
+	
 	//$sql = "INSERT INTO articles ('nom', 'description', 'categorie', 'sous_cat') VALUES('$nom', '$description', '$cat', '$sous_cat');";
-
-	$result = mysqli_query($dbh,$sql);
+	
 
 	if (!$result) 
 	{
