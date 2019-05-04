bbc46c71aa9261b28bc48885d89b49d631b8a985
sarahfng <sarahfong@outlook.fr>
2019-05-04 16:27:50 +0200

    pages_footer

---
+++ planete.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html> 
+<head> 
+	<title>ECE Amazon</title> 
+	<meta charset="utf-8" /> 
+	<link href="footer1.css" rel="stylesheet" type="text/css"/> 
+	<link href="https://fonts.googleapis.com/css?family=Yantramanav" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
+	
+	<script type="text/javascript" src="main.js"></script>
+</head> 
+ 
+<body>
+	<?php include ("navbar.php") ?>
+	<div id="contain2">
+
+	<h1 id="titre">ECE et notre planète</h1>
+   
+<div id="texte_equipe"><p> Chez Amazon, nous croyons que l'innovation a le pouvoir de changer le monde. Dans notre effort pour devenir la société la plus proche de ses clients au monde, nous sommes constamment à la recherche de nouvelles façons d'innover pour nos différents clients : particuliers effectuant leurs achats sur nos sites internationaux, vendeurs présents sur notre plate-forme, développeurs utilisant notre infrastructure pour créer leurs propres entreprises, auteurs des livres, musiques et films vendus sur nos sites. Nous estimons que notre plus importante contribution au bien de la société provient directement de ces activités fondamentales.
+<br>
+    <br>
+Amazon et l'environnement
+Chez Amazon, nous sommes constamment à la recherche de moyens visant à réduire notre impact sur l'environnement.
+<br>
+    <br>
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur semper ipsum a risus elementum mattis. Aliquam at pellentesque sem. Morbi blandit rhoncus convallis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Integer eget orci eu leo pulvinar laoreet euismod at elit. Morbi laoreet interdum sapien, quis rutrum ante tempor sed. Ut eu tincidunt lectus. Integer sagittis massa elit, at congue augue gravida vel. Aenean ut fermentum magna. Donec porttitor commodo euismod. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Proin egestas, magna in facilisis gravida, orci enim mattis magna, in pellentesque lectus sem sit amet leo. Ut nec nibh justo. Nunc id sem dapibus dolor ornare egestas vitae quis nisi. Fusce gravida enim sit amet felis posuere, vel sagittis leo varius.
+<br>
+    <br>
+Nam lectus massa, sollicitudin nec pellentesque at, ullamcorper non orci. In hac habitasse platea dictumst. Curabitur eu molestie ante. Maecenas vitae lectus ultricies, volutpat sapien posuere, ornare risus. Nulla posuere fermentum eros, eget efficitur dui tristique nec. Curabitur pharetra dui a pretium suscipit. Vivamus non dui feugiat, tincidunt ligula ac, posuere nibh. Nunc rhoncus leo et leo congue, ac efficitur risus pulvinar. Sed vitae magna ac sapien aliquam convallis. Sed eu efficitur mi, a egestas nibh. Aenean consectetur congue sapien, in gravida est laoreet id. Pellentesque elementum libero eros, nec ullamcorper nibh condimentum eget.
+<br>
+    <br>
+Vestibulum id ante mollis ipsum tristique egestas. Etiam bibendum lobortis lobortis. Suspendisse iaculis ipsum purus, eu feugiat elit porttitor id. Aliquam erat volutpat. Aenean sodales, augue sed tempor varius, sapien arcu pharetra nisi, eu venenatis sem lectus ut ex. Curabitur sollicitudin, mi a volutpat aliquam, nulla sem aliquam nisl, vel volutpat tortor nulla quis velit. Vestibulum tincidunt tempus semper. Quisque dictum blandit est vitae molestie.
+<br>
+    <br>
+Nullam semper velit vitae dolor congue, id consectetur sem gravida. Fusce in nulla vel massa varius ultrices. Vestibulum libero nisi, ornare at nunc sed, volutpat pellentesque libero. Vestibulum eleifend ante vel massa finibus gravida. Mauris hendrerit leo nec erat feugiat, ut mollis dolor imperdiet. Nam augue leo, feugiat quis dolor eu, lobortis luctus metus. Ut nec mi quam. Donec at ex est. Pellentesque sem ante, finibus eu dignissim eu, ornare vel metus. Etiam nisi justo, auctor in fringilla sit amet, finibus eget tellus. In molestie et felis quis ultricies. Phasellus id ex vitae tortor porttitor interdum vel at neque. Etiam sit amet dolor quis justo pellentesque laoreet. Fusce placerat at neque eget fringilla. Vestibulum nec fringilla nisi.
+<br>
+    <br>
+Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Maecenas sit amet ex luctus, sagittis eros eget, maximus sapien. Aliquam erat ligula, vestibulum ut pharetra vel, fringilla non neque. Nunc porttitor malesuada odio, et egestas turpis pellentesque eu. Fusce cursus vel purus nec tristique. Integer gravida sodales urna non convallis. Nullam tortor massa, congue ut accumsan lacinia, consectetur et turpis. Aliquam feugiat nibh vel felis varius, semper laoreet sem tincidunt. Aliquam mauris nisl, sagittis non ante sit amet, molestie tincidunt dolor. Vestibulum volutpat augue et erat consequat, hendrerit interdum enim tincidunt. Nullam condimentum consectetur tellus, ac varius justo mattis vel. Suspendisse efficitur fermentum elit, ac faucibus odio ultrices at. Etiam odio dolor, dictum sit amet est quis, aliquet tempor enim. Nam in dui elementum, posuere velit vehicula, egestas sapien. Fusce ex mauris, rhoncus sed sollicitudin ac, condimentum eget metus.
+<br>
+    <br>
+Cras nunc lacus, viverra at gravida ac, ultricies ut enim. Etiam nec nisi tellus. Sed egestas gravida nibh, id consectetur tellus elementum eu. Ut a mi ut enim tincidunt finibus vel at tortor. Nullam sed hendrerit sem. Nulla dignissim sed lacus auctor mattis. Vestibulum vulputate auctor erat, id tincidunt tellus aliquet sed. Aliquam vel rhoncus est. Nullam pellentesque, mauris sit amet ornare euismod, erat nulla finibus metus, nec iaculis odio magna ac nisl. Vestibulum suscipit ac libero vel aliquam. Nam congue massa at semper vehicula. Donec malesuada interdum venenatis. In hac habitasse platea dictumst. Nulla ac elit a magna interdum hendrerit ac vitae lacus. Vivamus vitae sem at tellus luctus sagittis.
+<br>
+    <br>
+Fusce porttitor faucibus nulla, ut venenatis tortor blandit vel. In ultricies mollis lorem, nec convallis magna sagittis vel. Pellentesque quis fringilla nunc. Sed pulvinar nisl ac ante efficitur consequat. Morbi rutrum, magna non posuere mollis, mi metus tempus sem, nec viverra augue ipsum id metus. Etiam nec lacus ultricies, tempor arcu a, scelerisque tellus. Quisque faucibus sit amet velit eu mollis. Cras urna lacus, rhoncus nec accumsan vel, tincidunt sed massa. Integer porta tincidunt erat id condimentum. Suspendisse venenatis lorem non porta rutrum. Cras pretium augue vitae est venenatis, ac tristique tellus aliquet. Vestibulum commodo, velit vitae faucibus aliquam, magna tortor efficitur nisl, non condimentum sem ligula ut orci. Phasellus sed nisi sem. Ut dignissim mauris vitae massa porta, a cursus lectus elementum. Nunc felis ligula, efficitur eget rutrum ut, rutrum quis elit.
+<br>
+    <br>
+Nunc condimentum, mauris nec pellentesque facilisis, augue enim feugiat turpis, non cursus justo magna et enim. Ut mi velit, finibus a facilisis eu, eleifend vitae tortor. Duis eu arcu in eros euismod ultrices. Fusce bibendum odio quis dolor rhoncus, id mollis justo faucibus. Curabitur eget nibh enim. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Phasellus suscipit nibh eros, a blandit ante scelerisque vel. In a nisl eget massa euismod convallis ac eu tortor. Pellentesque tincidunt sem sed feugiat ullamcorper. Nam mattis mi nec tellus gravida pharetra. Maecenas consectetur at libero et lacinia.
+<br>
+    <br>
+Vivamus sit amet efficitur diam. Proin quis pulvinar urna. Integer at lectus a ex porttitor posuere in a ante. Quisque aliquam eget odio non convallis. Nulla consectetur nisl sit amet nibh euismod interdum. Etiam nec efficitur turpis. Suspendisse malesuada dictum rhoncus. Proin non euismod augue. Nulla et tincidunt leo, vitae malesuada nibh. Mauris tempus pretium gravida. Maecenas sit amet elit quis arcu dignissim ullamcorper.
+<br>
+    <br>
+Morbi vel gravida tellus. Curabitur blandit lectus non rhoncus imperdiet. Nulla quis tristique sem. Vivamus eu dictum erat. Donec eu arcu enim. Ut lacus enim, tincidunt ut dui id, accumsan vehicula purus. Praesent eget tincidunt ligula. Sed non mauris non neque hendrerit dapibus varius sit amet enim. Aenean ex lectus, luctus at tincidunt eu, efficitur eget nisl. Nunc fermentum imperdiet nulla, eu elementum tortor consectetur at. Nunc faucibus et magna posuere finibus. Fusce luctus risus at ante lobortis posuere.
+<br>
+    <br>
+Pellentesque interdum sapien eu dui tempor eleifend. Aenean at convallis augue. Curabitur dolor ex, posuere et ornare vel, bibendum sit amet lectus. Integer dolor massa, malesuada eu orci sed, euismod iaculis purus. Mauris vehicula, sapien quis vehicula ultricies, nunc odio ultrices est, sit amet rutrum arcu augue vitae orci. Mauris non suscipit risus. Morbi in dolor et ex sodales congue nec nec lorem. Maecenas eget faucibus felis, et lobortis justo. Nullam hendrerit rhoncus leo, sit amet aliquam velit maximus euismod. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Quisque consequat augue urna, ut pellentesque augue rhoncus sit amet. Nunc vel neque neque. Maecenas quis varius diam. Pellentesque laoreet erat ligula, aliquam malesuada felis congue sed.
+<br>
+    <br>
+Duis eu lacinia nibh. Morbi mollis nisl sit amet eleifend dignissim. Suspendisse luctus magna ut rhoncus pretium. Proin sit amet velit faucibus, lobortis ante dignissim, tempor magna. Pellentesque et nibh ut mi blandit accumsan quis in lorem. In non ligula in metus maximus viverra. Nulla facilisi. Morbi rutrum ligula sit amet tortor posuere porttitor. Nullam rhoncus feugiat elit et ultricies. Vestibulum lectus erat, pretium quis tincidunt non, molestie quis est. Suspendisse potenti. Pellentesque lobortis metus vel congue rutrum. Fusce in ultrices massa.
+<br>
+    <br>
+Phasellus dignissim felis ut elementum porttitor. Nunc placerat sapien quis mi pulvinar, sit amet pharetra justo viverra. Curabitur volutpat porttitor sapien, ut mattis ligula blandit vel. Fusce tempor eros non mauris feugiat ultrices. Morbi ut lacus volutpat, eleifend nisl sed, aliquam libero. Sed egestas pulvinar aliquet. Vivamus ultrices dignissim laoreet. Praesent ultrices nisi vestibulum, elementum erat non, fermentum tellus. Donec interdum vel nisi ut euismod. Quisque augue arcu, aliquet non diam ut, finibus dapibus erat. Maecenas commodo interdum arcu, at mollis massa placerat eget. Praesent rutrum orci nisi, eu auctor ligula tincidunt a. Mauris scelerisque sit amet odio ut lobortis.
+<br>
+    <br>
+Ut sed tellus erat. Aenean vestibulum nulla et mi eleifend, eget tristique justo pulvinar. In sed enim sapien. Pellentesque sapien odio, viverra sed cursus non, fringilla id purus. Quisque sodales tempor leo, eget tristique odio egestas et. Quisque magna quam, rutrum eget est in, euismod euismod ligula. Duis maximus porta dapibus. Curabitur eget facilisis urna. Duis fringilla nulla massa, sed facilisis diam molestie vitae. Sed sodales dolor at odio egestas, sit amet viverra risus mattis. Phasellus ac nunc ac purus aliquet auctor. Praesent convallis, justo in tristique aliquam, est ex placerat mauris, in sollicitudin justo sapien non nunc.
+<br>
+    <br>
+Phasellus nec blandit lacus. Duis congue eu massa ut mattis. Pellentesque molestie ut enim vitae tempor. Integer ultricies fermentum felis, at auctor libero aliquam a. Vestibulum ipsum arcu, volutpat sed libero eu, aliquet feugiat tellus. Pellentesque sagittis pulvinar dui eget sollicitudin. Nam pellentesque, ante vitae euismod porta, leo odio scelerisque elit, eget tristique velit metus sed felis. Fusce ipsum turpis, fermentum eget est non, luctus malesuada sem. Sed non facilisis nisi, eget tristique odio. Pellentesque eleifend diam dignissim accumsan facilisis. Nam et mi velit. Duis commodo ultrices luctus. Suspendisse potenti. Maecenas viverra eleifend tortor, in placerat mi eleifend vitae. Nulla tempor iaculis commodo. In congue posuere magna, in pellentesque lacus bibendum sed.
+<br>
+</p></div>
+
+</div>
+	<?php include ("footer.php") ?>
+</body>
+    
